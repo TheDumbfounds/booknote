a10e61d302e56c6d91797ff206f77b4daf59dc02
Rechunk <gabri.marc@gmx.net>
2018-02-15 19:15:25 +0100

    url refactoring, styles changed

--- books/urls.py
+++ books/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'books'
 urlpatterns = [
-    path('books/add/', views.BookAddView.as_view(), name='book-add'),
-    path('books/<slug:slug>', views.BookDetailView.as_view(), name='book-detail'),
-    path('books/', views.BookListView.as_view(), name='booklist'),
+    path('books/<slug:slug>/', include([
+        path('add', views.BookCreateView.as_view(), name='add'),
+        path('delete', views.BookDeleteView.as_view(), name='delete'),
+        path('', views.BookDetailView.as_view(), name='detail'),
+    ])),
+    path('books/', views.BookListView.as_view(), name='list'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from .models import Book
@@ -17,12 +18,21 @@ class BookDetailView(DetailView):
     model = Book
     template_name = 'books/bookdetail.html'
     context_object_name = 'book'
-    slug_url_kwarg = 'slug'
 
 
 @method_decorator(login_required, name='dispatch')
-class BookAddView(CreateView):
+class BookCreateView(CreateView):
     model = Book
     template_name = 'books/add-book.html'
     success_url = '/books'
     fields = '__all__'
+
+
+@method_decorator(login_required, name='dispatch')
+class BookDeleteView(DeleteView):
+    model = Book
+    fields = '__all__'
+    success_url = '/books'
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
