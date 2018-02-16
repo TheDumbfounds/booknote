3e2e59a8aa5c310c79a9fd87b1372e933497cd51
Rechunk <gabri.marc@gmx.net>
2018-02-16 15:31:50 +0100

    CustomNoteForm added

--- books/forms.py
+++ books/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Book
+from .models import Book, Note
 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+
+
+class CustomNoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ('title', 'body')
