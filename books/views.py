fad88135059f4f598a24fcb869f384122baab4c8
Rechunk <gabri.marc@gmx.net>
2018-02-16 15:32:19 +0100

    post function in details view to handle new notes

--- books/views.py
+++ books/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from .models import Book
+from .models import Book, Note
+from .forms import CustomNoteForm
 
 
 @method_decorator(login_required, name='dispatch')
@@ -18,6 +19,29 @@ class BookDetailView(DetailView):
     model = Book
     template_name = 'books/bookdetail.html'
     context_object_name = 'book'
+
+    def post(self, request, *args, **kwargs):
+        form = CustomNoteForm(request.POST)
+        if form.is_valid():
+            book = get_object_or_404(Book, slug=kwargs['slug'])
+            title = form.cleaned_data['title']
+            body = form.cleaned_data['body']
+
+            Note.objects.create(
+                title=title,
+                body=body,
+                author=request.user,
+                book=book,
+            ).save()
+
+        return redirect(reverse('books:detail', kwargs={'slug': kwargs['slug']}))
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetailView, self).get_context_data(**kwargs)
+        context['form'] = CustomNoteForm()
+        book = get_object_or_404(Book, slug=self.kwargs['slug'])
+        context['notes'] = Note.objects.filter(book=book)
+        return context
 
 
 @method_decorator(login_required, name='dispatch')
