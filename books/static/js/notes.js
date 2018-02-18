ae92222dd731947e11f8e1c446026e5cd26d19db
Rechunk <gabri.marc@gmx.net>
2018-02-18 19:10:37 +0100

    delete notes function (backend)

--- books/static/js/notes.js
+++ books/static/js/notes.js
@@ -1,1 +1,17 @@
 $('.modal').modal()
+function deleteNote(slug, id){
+  $.ajax({
+    method: 'DELETE',
+    url: slug,
+    data: {
+      'note_id': id,
+    },
+    beforeSend: function(xhr){
+      xhr.setRequestHeader('X-CSRFToken', csrf_token)
+    },
+    success: function(){
+      console.log("OKAY")
+    },
+    error: function(err){console.log(err)}
+  })
+}
