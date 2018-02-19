1871a46c0f4f6957e061df5ecbdda954145b4896
Rechunk <gabri.marc@gmx.net>
2018-02-19 09:32:06 +0100

    note_id optional field for editing notes

--- books/forms.py
+++ books/forms.py
@@ -3,6 +3,8 @@ from .models import Book, Note
 
 
 class CustomNoteForm(forms.ModelForm):
+    note_id = forms.CharField(required=False, widget=forms.HiddenInput())
+
     class Meta:
         model = Note
         fields = ('title', 'body')
