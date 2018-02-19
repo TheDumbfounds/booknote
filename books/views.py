8df17f5c00c5673fc3de892c9cb351ed9533d3ec
Rechunk <gabri.marc@gmx.net>
2018-02-19 09:32:57 +0100

    .

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.template.loader import render_to_string
 from django.http import HttpResponse, QueryDict
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
@@ -15,6 +16,9 @@ class BookListView(ListView):
     template_name = 'books/booklist.html'
     context_object_name = 'books'
 
+    def get_queryset(self):
+        return Book.objects.filter(added_by=self.request.user)
+
 
 @method_decorator(login_required, name='dispatch')
 class BookDetailView(DetailView):
@@ -26,21 +30,28 @@ class BookDetailView(DetailView):
         form = CustomNoteForm(request.POST)
         if form.is_valid():
             book = get_object_or_404(Book, slug=kwargs['slug'])
+            note_id = form.cleaned_data['note_id']
             title = form.cleaned_data['title']
             body = form.cleaned_data['body']
 
-            Note.objects.create(
-                title=title,
-                body=body,
-                author=request.user,
-                book=book,
-            ).save()
+            if note_id:
+                note = get_object_or_404(Note, added_by=request.user, id=note_id)
+                note.title = title
+                note.body = body
+                note.save()
+            else:
+                Note.objects.create(
+                    title=title,
+                    body=body,
+                    added_by=request.user,
+                    book=book,
+                ).save()
 
         return redirect(reverse('books:detail', kwargs={'slug': kwargs['slug']}))
 
     def delete(self, request, *args, **kwargs):
         note_id = QueryDict(request.body).get('note_id')
-        note = get_object_or_404(Note, author=request.user, id=note_id)
+        note = get_object_or_404(Note, added_by=request.user, id=note_id)
         note.delete()
         return HttpResponse('')
 
@@ -59,6 +70,11 @@ class BookCreateView(CreateView):
     success_url = '/books'
     fields = ('name', 'author', 'rating')
 
+    def get_form(self, *args, **kwargs):
+        form = super(BookCreateView, self).get_form(*args, **kwargs)
+        form.instance.added_by = self.request.user
+        return form
+
 
 @method_decorator(login_required, name='dispatch')
 class BookDeleteView(DeleteView):
@@ -68,3 +84,14 @@ class BookDeleteView(DeleteView):
 
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
+
+
+def render_modal(request, note_id=''):
+    if note_id:
+        note = get_object_or_404(Note, id=note_id)
+        form = CustomNoteForm(initial={'title': note.title, 'body': note.body})
+    else:
+        form = CustomNoteForm()
+
+    csrf_token = request.GET['csrf_token']
+    return HttpResponse(render_to_string('books/_modal_inner.html', {'form': form, 'csrf_token': csrf_token, 'note_id': note_id}))
