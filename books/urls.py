e9a4ff5044a13a532c677a3bb7ad8bb7812e993a
Rechunk <gabri.marc@gmx.net>
2018-02-19 09:32:34 +0100

    path added to fetch modal dynamically

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'books'
 urlpatterns = [
     path('books/', include([
+        path('modal/<int:note_id>', views.render_modal, name='modal'),
+        path('modal/', views.render_modal, name='modal'),
         path('', views.BookListView.as_view(), name='list'),
         path('new', views.BookCreateView.as_view(), name='add'),
         path('<slug:slug>', include([
